f52db50c3c4986e62e0285b1263ac9c59ef2c070
JIGNESH PATEL <jignesh.mahesh07@gmail.com>
2021-11-06 23:51:58 +0530

    Section 16 -> 02 Making category request to backend

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth/helper";
 import Base from "../core/Base";
 
 const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAuthenticated();
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link className="btn btn-small btn-info mb-3" to="/admin/dashboard">
+        Admin Home
+      </Link>
+    </div>
+  );
+
+  const myCategoryForm = () => (
+    <form>
+      <div className="form-group">
+        <p className="lead">Enter the category</p>
+        <input
+          type="text"
+          className="form-control my-3"
+          autoFocus
+          required
+          placeholder="For Ex. Summer"
+        />
+        <button className="btn btn-outline-info">Create category</button>
+      </div>
+    </form>
+  );
+
   return (
     <Base
       title="Create a category here"
@@ -10,7 +42,7 @@ const AddCategory = () => {
     >
       <div className="row bg-white rounded">
         <div className="col-md-8 offset-md-2">
-          <h1>Hello</h1>
+          {myCategoryForm()} {goBack()}
         </div>
       </div>
     </Base>
